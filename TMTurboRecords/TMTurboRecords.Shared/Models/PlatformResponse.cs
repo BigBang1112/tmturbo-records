21c89bf1b937142e45ed8e6fc9abf61c89ce2cf1
Petr Pivoňka <petrpiv1@gmail.com>
2024-05-21 19:47:42 +0200

    Do not display unrertieved count

--- TMTurboRecords/TMTurboRecords.Shared/Models/PlatformResponse.cs
+++ TMTurboRecords/TMTurboRecords.Shared/Models/PlatformResponse.cs
@@ -2,7 +2,7 @@
 
 public sealed class PlatformResponse
 {
-    public required int Count { get; set; }
+    public required int? Count { get; set; }
     public required DateTimeOffset? Timestamp { get; set; }
     public required string? Error { get; set; }
 }
